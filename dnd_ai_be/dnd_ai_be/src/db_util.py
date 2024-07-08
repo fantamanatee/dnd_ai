abe736716483cbbbecea8e97f9f8fdceb29e8c92
Aaron <suaaron55@gmail.com>
2024-07-08 15:53:24 -0400

    retry, old uri, different IP allowed for monogodb

--- dnd_ai_be/dnd_ai_be/src/db_util.py
+++ dnd_ai_be/dnd_ai_be/src/db_util.py
@@ -1,17 +1,26 @@
 from pymongo import MongoClient
-import ssl
+# import ssl
+# import os
+
+# # URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/?appName=Cluster91339"
+# URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/test_db?retryWrites=true&w=majority"
+
+
+# URI_with_options = f"{URI}&ssl=true&connectTimeoutMS=30000&socketTimeoutMS=30000"
+
+# # CLIENT = MongoClient(URI)
+# CLIENT = MongoClient(URI_with_options, tlsAllowInvalidCertificates=True)
+
+# DB_NAME = 'test_db'
+# DB = CLIENT[DB_NAME]
+
+# # TODO close client during long periods of inactivity.
+
+
 import os
 
-# URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/?appName=Cluster91339"
-URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/test_db?retryWrites=true&w=majority"
-
-
+URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/?appName=Cluster91339"
 URI_with_options = f"{URI}&ssl=true&connectTimeoutMS=30000&socketTimeoutMS=30000"
-
-# CLIENT = MongoClient(URI)
-CLIENT = MongoClient(URI_with_options, tlsAllowInvalidCertificates=True)
-
+CLIENT = MongoClient(URI_with_options)
 DB_NAME = 'test_db'
 DB = CLIENT[DB_NAME]
-
-# TODO close client during long periods of inactivity.
