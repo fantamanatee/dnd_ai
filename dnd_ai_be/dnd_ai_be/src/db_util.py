e8050b1549160174842fef8b624a147b349b09da
Aaron <suaaron55@gmail.com>
2024-07-08 15:45:17 -0400

    increase timeout

--- dnd_ai_be/dnd_ai_be/src/db_util.py
+++ dnd_ai_be/dnd_ai_be/src/db_util.py
@@ -6,7 +6,7 @@ import os
 URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/test_db?retryWrites=true&w=majority"
 
 
-URI_with_options = f"{URI}&ssl=true&connectTimeoutMS=10000&socketTimeoutMS=10000"
+URI_with_options = f"{URI}&ssl=true&connectTimeoutMS=30000&socketTimeoutMS=30000"
 
 # CLIENT = MongoClient(URI)
 CLIENT = MongoClient(URI_with_options, tlsAllowInvalidCertificates=True)
