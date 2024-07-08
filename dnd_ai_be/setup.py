f9993c2d90491934ab635dda0fad60e8deb1c0ce
Aaron <suaaron55@gmail.com>
2024-07-08 13:18:50 -0400

    set python=3.10

--- dnd_ai_be/setup.py
+++ dnd_ai_be/setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dnd_ai_be',
+    python='3.10',
     version='0.1',
     packages=find_packages(include=['dnd_ai_be', 'dnd_ai_be.*']),
     install_requires=[
