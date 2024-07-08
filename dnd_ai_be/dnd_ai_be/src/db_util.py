36b05e56c0f9ed3556db7b4509679c72f928faf9
Aaron <suaaron55@gmail.com>
2024-07-08 14:54:48 -0400

    add uri options

--- dnd_ai_be/dnd_ai_be/src/db_util.py
+++ dnd_ai_be/dnd_ai_be/src/db_util.py
@@ -8,7 +8,11 @@ from typing import Union, List
 import os
 
 URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/?appName=Cluster91339"
-CLIENT = MongoClient(URI)
+URI_with_options = f"{URI}&ssl=true&connectTimeoutMS=30000&socketTimeoutMS=30000"
+
+# CLIENT = MongoClient(URI)
+CLIENT = MongoClient(URI_with_options)
+
 DB_NAME = 'test_db'
 DB = CLIENT[DB_NAME]
 
