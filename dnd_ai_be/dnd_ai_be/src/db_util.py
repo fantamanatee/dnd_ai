411cb48907f434ddc859eaf4bbaec50139b0ffa7
Aaron <suaaron55@gmail.com>
2024-07-08 15:07:27 -0400

    remove ssl certs

--- dnd_ai_be/dnd_ai_be/src/db_util.py
+++ dnd_ai_be/dnd_ai_be/src/db_util.py
@@ -1,20 +1,15 @@
 from pymongo import MongoClient
-from dnd_ai_be.src.characters import Entity, NPC, Player
-
-# from langchain_core.load.serializable import Serializable
-# from langchain_core.documents import Document
-
-from typing import Union, List
+import ssl
 import os
 
 # URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/?appName=Cluster91339"
 URI = f"mongodb+srv://{os.getenv('DND_AI_DB_USER')}:{os.getenv('DND_AI_DB_PWD')}@cluster91339.czkmuen.mongodb.net/test_db?retryWrites=true&w=majority"
 
 
-URI_with_options = f"{URI}&ssl=true&connectTimeoutMS=30000&socketTimeoutMS=30000"
+URI_with_options = f"{URI}&ssl=true&connectTimeoutMS=10000&socketTimeoutMS=10000"
 
 # CLIENT = MongoClient(URI)
-CLIENT = MongoClient(URI_with_options)
+CLIENT = MongoClient(URI_with_options, ssl_cert_reqs=ssl.CERT_NONE)
 
 DB_NAME = 'test_db'
 DB = CLIENT[DB_NAME]
