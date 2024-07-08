8821a0dd285fa3f54897b2545b19002eb4252c72
Aaron <suaaron55@gmail.com>
2024-07-08 13:43:35 -0400

    BE -- try with specific versions

--- dnd_ai_be/setup.py
+++ dnd_ai_be/setup.py
@@ -5,34 +5,49 @@ setup(
     version='0.1',
     packages=find_packages(include=['dnd_ai_be', 'dnd_ai_be.*']),
     install_requires=[
-        'Flask',
-        'langchain',
-        'langchain_chroma',
-        'langchain_community',
-        'langchain_openai',
-        'langchain_text_splitters',
-        'langchain_mongodb',
-        'pymongo',
-        'flask-cors',
-        'motor',
-        'beautifulsoup4',
+        'beautifulsoup4==4.12.3',
+        'Flask==3.0.3',
+        'Flask_Cors==4.0.1',
+        'flask_swagger_ui==4.11.1',
+        'langchain==0.2.6',
+        'langchain_chroma==0.1.2',
+        'langchain_community==0.2.6',
+        'langchain_core==0.2.11',
+        'langchain_mongodb==0.1.6',
+        'langchain_openai==0.1.14',
+        'langchain_text_splitters==0.2.2',
+        'pymongo==4.8.0',
+        'python-dotenv==1.0.1',
+        'setuptools==69.5.1',
     ],
 )
 
-# beautifulsoup4==4.12.3
-# Flask==3.0.3
-# Flask_Cors==4.0.1
-# flask_swagger_ui==4.11.1
-# langchain==0.2.6
-# langchain_chroma==0.1.2
-# langchain_community==0.2.6
-# langchain_core==0.2.11
-# langchain_mongodb==0.1.6
-# langchain_openai==0.1.14
-# langchain_text_splitters==0.2.2
-# pymongo==4.8.0
-# python-dotenv==1.0.1
-# setuptools==69.5.1
+# 'beautifulsoup4==4.12.3',
+# 'Flask==3.0.3',
+# 'Flask_Cors==4.0.1',
+# 'flask_swagger_ui==4.11.1',
+# 'langchain==0.2.6',
+# 'langchain_chroma==0.1.2',
+# 'langchain_community==0.2.6',
+# 'langchain_core==0.2.11',
+# 'langchain_mongodb==0.1.6',
+# 'langchain_openai==0.1.14',
+# 'langchain_text_splitters==0.2.2',
+# 'pymongo==4.8.0',
+# 'python-dotenv==1.0.1',
+# 'setuptools==69.5.1',
+
+        # 'Flask',
+        # 'langchain',
+        # 'langchain_chroma',
+        # 'langchain_community',
+        # 'langchain_openai',
+        # 'langchain_text_splitters',
+        # 'langchain_mongodb',
+        # 'pymongo',
+        # 'flask-cors',
+        # 'motor',
+        # 'beautifulsoup4',
 
 # Flask
 # langchain
