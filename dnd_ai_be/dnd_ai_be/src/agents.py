bb6b3ce632ed8899eac62110b01cf0ec37d65f82
Aaron <suaaron55@gmail.com>
2024-07-31 13:54:26 -0400

    BE -- agents functionality (WIP)

--- dnd_ai_be/dnd_ai_be/src/agents.py
+++ dnd_ai_be/dnd_ai_be/src/agents.py
@@ -1,22 +1,48 @@
 # Inspired by https://arxiv.org/pdf/2304.03442, which uses observations, planning, and reflections to simulate agents.
 # We will only use observations and reflections for our role-play agents.
 
+from typing import List, Dict
+from dnd_ai_be.src.bots import ChatBot, ReasoningBot
+from dnd_ai_be.src.characters import Player, NPC, Entity
+from bson import ObjectId
+from dnd_ai_be.src.db_util import DB, URI, DB_NAME
+
 
 class Agent:
-    def __init__(self, reasoning_bots, db_collection):
+    def __init__(
+        self,
+        name: str = None,
+        reasoning_bots: Dict[ReasoningBot, int] = None,
+        chat_bots: List[ChatBot] = None,
+        ID: str = None,
+    ):
         """
         Initializes the Agent with reasoning bots and a database collection.
 
         Args:
-            reasoning_bots (dict): Dictionary mapping bots to their application frequency.
-            db_collection (MongoDBCollection): The MongoDB collection for storing observations and reflections.
+            reasoning_bots (dict): Dictionary mapping reasoning bots to their call frequency.
+            memory_stream (MongoDBCollection): The MongoDB collection for storing observations and reflections.
+            ID (str): The unique identifier for the Agent.
         """
+        self.name = name
         self.reasoning_bots = reasoning_bots
-        self.col = db_collection
+        self.chat_bots = chat_bots
         self.turn_count = 0
-        self.history = []
 
-    def handle_input(self, input_text, prompter, responder, chatbot_response):
+        # FIXME persist agents in DB.Agents?
+        # if not ID is None:
+        #     if self.col.find_one({"_id": ObjectId(ID)}) is None:
+        #         raise ValueError("BOT_ID does not exist in database.")
+        #     else:
+        #         self.ID = ID
+        #         self.name = self.col.find_one({"_id": ObjectId(ID)})["name"]
+        #         self.reasoning_bots = self.col.find_one({"_id": ObjectId(ID)})["reasoning_bots"]
+        #         self.chat_bots = self.col.find_one({"_id": ObjectId(ID)})["chat_bots"]
+
+        #         print("Chatbot loaded with BOT_ID:", ID)
+        # else:
+
+    def handle_input(self, input_text, prompter, responder):
         """
         Handles user input and determines the appropriate bot to generate responses.
 
@@ -31,22 +57,26 @@ class Agent:
         """
         self.turn_count += 1
 
+        chatbot = self.chat_bots[0]  # FIXME make this dynamic later
+        chatbot_response = chatbot.generate_response(input_text, prompter, responder)
+
         # Store the chatbot response in the history
-        self.history.append(chatbot_response)
 
         # Select the reasoning bots based on the turn count
         selected_reasoning_bots = self.select_reasoning_bots()
 
         # Generate reasoning responses (observations and/or reflections)
         reasoning_responses = []
-        for bot in selected_reasoning_bots:
-            response = bot.generate_response(self.history)
+        for reasoning_bot in selected_reasoning_bots:
+            k = self.reasoning_bots[reasoning_bot]
+            response = reasoning_bot.generate_response(
+                input_text, prompter, responder, k
+            )
             reasoning_responses.append(response)
-            self.store_reasoning_response(response)
 
         return chatbot_response, reasoning_responses
 
-    def select_reasoning_bots(self):
+    def select_reasoning_bots(self) -> List[ReasoningBot]:
         """
         Selects the appropriate reasoning bots based on the turn count.
 
@@ -60,18 +90,42 @@ class Agent:
 
         return selected_bots
 
-    def store_reasoning_response(self, response):
-        """
-        Stores the reasoning response in the database.
-
-        Args:
-            response (str): The reasoning response to be stored.
-        """
-        self.col.insert_one({"turn": self.turn_count, "response": response})
-
     def reset(self):
         """
         Resets the agent for a new session.
         """
         self.turn_count = 0
         self.history = []
+
+
+if __name__ == "__main__":
+    # Initialize the Agent
+    observation_bot = ReasoningBot(
+        name="ObservationBot1",
+        reasoning_collection_name="Sessions",
+        reasoning_system_prompt="Use the most recent chat entry to generate a new observation about the character(s). Try to make inferences rather than simply repeating information. Be direct and brief. Use NER and to identify entities and relationships. \n\n{context}",
+    )
+    reflection_bot = ReasoningBot(
+        name="ReflectionBot1",
+        reasoning_collection_name="Memories",
+        reasoning_system_prompt="Use the provided observations to generate a new reflection about the character(s) and their actions/ dialogue. Try to make inferences rather than simply repeating information, and try to incorporate all significant details in your reflection. Write a one sentence summary of the provided . Use NER to identify entities and relationships. \n\n{context}",
+    )
+
+    # observation_bot = ReasoningBot(ID="669fe418fa6996a3780c1531")
+    reasoning_bots = {
+        observation_bot: 1,
+        reflection_bot: 4,
+    }
+
+    NER1 = ChatBot(ID="6696c90e56b00a88e374791a")
+    chat_bots = [NER1]
+
+    agent = Agent(name="agent1", reasoning_bots=reasoning_bots, chat_bots=chat_bots)
+    prompter = Player(ID="66997d89afca9066c9e31804")  # Legolas
+    responder = NPC(ID="66997d86afca9066c9e31803")  # Gobbo
+
+    while True:
+        prompt = input("Enter a prompt: ")
+        response, reasoning = agent.handle_input(prompt, prompter, responder)
+        print("response:", response)
+        print("reasoning:", reasoning)
--- dnd_ai_be/dnd_ai_be/src/bots.py
+++ dnd_ai_be/dnd_ai_be/src/bots.py
@@ -4,18 +4,42 @@ from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
+import json
+from langchain_core.messages import (
+    BaseMessage,
+    message_to_dict,
+    messages_from_dict,
+)
+from langchain_core.messages.ai import AIMessage
+
+from langchain_core.runnables.utils import (
+    ConfigurableFieldSpec,
+)
+
+from dnd_ai_be.src.history import RunnableWithMessageHistory
+
+# from langchain_core.runnables.history import RunnableWithMessageHistory
+
+from dnd_ai_be.src.mongo_chat_history import MongoDBChatMessageHistory
+
+# from langchain_mongodb.chat_message_histories import MongoDBChatMessageHistory
 from bson import ObjectId
 from pprint import pprint
 from dnd_ai_be.src.db_util import DB, URI, DB_NAME
-from dnd_ai_be.src.characters import Entity, Player, NPC
+from dnd_ai_be.src.characters import Entity, Player, NPC, EntityLike
 from dnd_ai_be.src.util import timer
 
+import openai
+from langsmith.wrappers import wrap_openai
+from langsmith import traceable
 
-class Chatbot:
+# Auto-trace LLM calls in-context
+client = wrap_openai(openai.Client())
+
+
+class ChatBot:
     """
     Chatbot class that wraps the OpenAI API and provides a simple interface for interacting with it.
     """
@@ -72,12 +96,13 @@ class Chatbot:
             }
             result = self.col.insert_one(chatbot_data)
             self.ID = result.inserted_id
+            print("ChatBot created with BOT_ID:", ID)
 
         self.contextualize_q_prompt = ChatPromptTemplate.from_messages(
             [
                 ("system", contextualize_q_system_prompt),
                 MessagesPlaceholder("history"),
-                ("human", "{input}"),
+                ("human", "({input}"),
             ]
         )
 
@@ -85,7 +110,7 @@ class Chatbot:
             [
                 ("system", qa_system_prompt),
                 MessagesPlaceholder("history"),
-                ("human", "{input}"),
+                ("human", "({input}"),
             ]
         )
 
@@ -99,7 +124,10 @@ class Chatbot:
     def get_name(self) -> str:
         return self.col.find_one({"_id": self.ID})["name"]
 
-    def qa(self, input: str, prompter: Entity, responder: Entity) -> str:
+    @traceable
+    def generate_response(
+        self, input_text: str, prompter: EntityLike, responder: EntityLike
+    ) -> str:
         """Perform QA chain.
         Args:
             prompter_input: The input for the prompter.
@@ -115,29 +143,50 @@ class Chatbot:
         prompter_context = f"PROMPTER_CONTEXT:\n{prompter.get_context_str()}"
         responder_context = f"RESPONDER_CONTEXT:\n{responder.get_context_str()}"
 
-        @timer()
-        def create_retriever():
-            text_splitter = RecursiveCharacterTextSplitter(
-                chunk_size=1000, chunk_overlap=200
-            )
-            splits = text_splitter.create_documents(
-                [prompter_context, responder_context]
-            )
-            vectorstore = Chroma.from_documents(
-                documents=splits, embedding=OpenAIEmbeddings()
-            )
-            retriever = vectorstore.as_retriever()
-            history_aware_retriever = create_history_aware_retriever(
-                self.llm, retriever, self.contextualize_q_prompt
-            )
-            return history_aware_retriever
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
+        splits = text_splitter.create_documents([prompter_context, responder_context])
+        vectorstore = Chroma.from_documents(
+            documents=splits, embedding=OpenAIEmbeddings()
+        )
+        retriever = vectorstore.as_retriever()
+        self.history_aware_retriever = create_history_aware_retriever(
+            self.llm, retriever, self.contextualize_q_prompt
+        )
 
-        if self.history_aware_retriever is None or NEW_SESSION:
-            self.history_aware_retriever = create_retriever()
+        # if self.history_aware_retriever is None or NEW_SESSION:
+        #     self.history_aware_retriever = create_retriever()
 
         rag_chain = create_retrieval_chain(
             self.history_aware_retriever, self.question_answer_chain
         )
+
+        # with_message_history = RunnableWithMessageHistory(
+        #     rag_chain,
+        #     get_session_history=get_session_history,
+        #     input_messages_key="question",
+        #     history_messages_key="history",
+        #     history_factory_config=[
+        #         ConfigurableFieldSpec(
+        #             id="user_id",
+        #             annotation=str,
+        #             name="User ID",
+        #             description="Unique identifier for the user.",
+        #             default="",
+        #             is_shared=True,
+        #         ),
+        #         ConfigurableFieldSpec(
+        #             id="conversation_id",
+        #             annotation=str,
+        #             name="Conversation ID",
+        #             description="Unique identifier for the conversation.",
+        #             default="",
+        #             is_shared=True,
+        #         ),
+        #     ],
+        # )
+
         print(f"session_id: {self.session_id}")
         conversational_rag_chain = RunnableWithMessageHistory(
             rag_chain,
@@ -150,19 +199,19 @@ class Chatbot:
             input_messages_key="input",
             history_messages_key="history",
             output_messages_key="answer",
+            input_prefix=prompter.get_name(),
+            output_prefix=responder.get_name(),
         )
 
-        @timer()
-        def do_invoke():
-            res = conversational_rag_chain.invoke(
-                {"input": input},
-                config={"configurable": {"session_id": self.session_id}},
-            )
-            return res
-
-        res = do_invoke()
+        res = conversational_rag_chain.invoke(
+            input={"input": input_text, "name": prompter.get_name()},
+            config={
+                "configurable": {
+                    "session_id": self.session_id,
+                }
+            },
+        )
         answer = res["answer"]
-
         return answer
 
     def to_dict(self):
@@ -170,23 +219,165 @@ class Chatbot:
         return data
 
 
+class ReasoningBot:
+    """
+    ReasoningBot class that wraps the OpenAI API and provides a simple interface for interacting with it.
+    """
+
+    def __init__(
+        self,
+        name: str = None,
+        reasoning_collection_name: str = None,
+        reasoning_system_prompt: str = None,
+        config: dict = None,
+        ID: str = None,
+    ) -> None:
+        """Initialize the ReasoningBot object.
+        Args:
+            reasoning_system_prompt: A system prompt for the reasoning system.
+            config: A ChatbotConfig object containing the configuration for the chatbot.
+            ID: A unique identifier for the chatbot.
+        """
+        ID = ObjectId(ID) if ID else None
+        DEFAULT_CONFIG = {
+            "model": "gpt-3.5-turbo",
+            "temperature": 0,
+        }  # default config
+
+        self.name = name
+        if config is None:
+            config = DEFAULT_CONFIG
+        self.config = config
+        self.col = DB.Bots
+        self.mem_col = DB.Memories
+        self.reasoning_collection_name = reasoning_collection_name
+        self.session_id = None
+
+        if not ID is None:
+            if self.col.find_one({"_id": ObjectId(ID)}) is None:
+                raise ValueError("BOT_ID does not exist in database.")
+            else:
+                self.ID = ID
+                reasoning_system_prompt = self.col.find_one({"_id": ObjectId(ID)})[
+                    "reasoning_system_prompt"
+                ]
+                config = self.col.find_one({"_id": ObjectId(ID)})["config"]
+                print("ReasoningBot loaded with BOT_ID:", ID)
+        else:
+            reasoning_bot_data = {
+                "name": name,
+                "reasoning_system_prompt": reasoning_system_prompt,
+                "config": config,
+            }
+            result = self.col.insert_one(reasoning_bot_data)
+            self.ID = result.inserted_id
+            print("ReasoningBot created with BOT_ID:", self.ID)
+
+        self.reasoning_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", reasoning_system_prompt),
+                ("human", "{input}"),
+            ]
+        )
+
+        self.llm = ChatOpenAI(
+            model=self.config["model"], temperature=self.config["temperature"]
+        )
+        self.question_answer_chain = create_stuff_documents_chain(
+            self.llm, self.reasoning_prompt
+        )
+
+    def get_name(self) -> str:
+        return self.col.find_one({"_id": self.ID})["name"]
+
+    def generate_response(
+        self, input_text: str, prompter: EntityLike, responder: EntityLike, k: int = 1
+    ) -> str:
+        """Perform reasoning chain.
+        Args:
+            input: The input for the prompter.
+            prompter: The prompter entity.
+            responder: The responder entity.
+            k: The number of previous responses to consider for reasoning.
+        """
+
+        print(f"Prompter: {prompter.get_name()}, Responder: {responder.get_name()}")
+        cur_session_id = str(prompter.ID) + str(responder.ID)
+        NEW_SESSION = False
+        if self.session_id is None or self.session_id != cur_session_id:
+            self.session_id = cur_session_id
+            NEW_SESSION = True
+
+        @timer()
+        def get_last_k_msgs(session_id, k):
+            message_history = MongoDBChatMessageHistory(
+                session_id=session_id,
+                connection_string=URI,
+                database_name=DB_NAME,
+                collection_name=self.reasoning_collection_name,
+            )
+            history = message_history.messages
+            return history[-2 * k :] if len(history) >= 2 * k else history
+
+        last_k_msgs = get_last_k_msgs(self.session_id, k)
+        reasoning_context = "\n".join([msg.content for msg in last_k_msgs])
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200
+        )
+        context_documents = text_splitter.create_documents([reasoning_context])
+
+        res = self.question_answer_chain.invoke(
+            input={
+                "input": input_text,
+                "name": prompter.get_name(),
+                "context": context_documents,
+            },
+            config={"configurable": {"session_id": self.session_id}},
+        )
+
+        # send response to Memories collection
+        message = AIMessage(content=res)
+        data = {
+            "SessionId": self.session_id,
+            "BotName": self.get_name(),
+            "History": json.dumps(message_to_dict(message)),
+        }
+        self.mem_col.insert_one(data)
+        return res
+
+    def to_dict(self):
+        data = self.col.find_one({"_id": self.ID})
+        return data
+
+
 if __name__ == "__main__":
-    bot_args = {
-        "name": "default_bot_no_context_q_sys_prompt2",
-        "contextualize_q_system_prompt": "Given a chat history and the latest prompt which might reference context in the chat history, formulate a standalone prompt which can be understood without the chat history. Do NOT answer the prompt, just reformulate it if needed and otherwise return it as is.",
-        "qa_system_prompt": "You are a role playing chatbot for a Dungeons and Dragons game. There are two fictional characters: a prompter and a responder. You must respond to the prompt as if you are the responder. Use the following pieces of retrieved context to answer the prompt. If the context does not apply, respond in a way that makes sense. Use three sentences maximum, and keep the answer concise.\n"
-        + "\n"
-        + "{context}",
-        "config": {"model": "gpt-3.5-turbo", "temperature": 0},
-    }
-    bot = Chatbot(**bot_args)
+    # bot_args = {
+    #     "name": "default_bot_no_context_q_sys_prompt2",
+    #     "contextualize_q_system_prompt": "Given a chat history and the latest prompt which might reference context in the chat history, formulate a standalone prompt which can be understood without the chat history. Do NOT answer the prompt, just reformulate it if needed and otherwise return it as is.",
+    #     "qa_system_prompt": "You are a role playing chatbot for a Dungeons and Dragons game. There are two fictional characters: a prompter and a responder. You must respond to the prompt as if you are the responder. Use the following pieces of retrieved context to answer the prompt. If the context does not apply, respond in a way that makes sense. Use three sentences maximum, and keep the answer concise.\n"
+    #     + "\n"
+    #     + "{context}",
+    #     "config": {"model": "gpt-3.5-turbo", "temperature": 0},
+    # }
+    # bot = ChatBot(**bot_args)
 
-    print("bot:", bot.get_name())
+    # print("bot:", bot.get_name())
 
-    prompter = Player(ID=ObjectId("668b02bdf19ba1d40bb92630"))
-    responder = NPC(ID=ObjectId("668ca09b8b1a765fbab94e9c"))
+    # prompter = Player(ID=ObjectId("668b02bdf19ba1d40bb92630"))
+    # responder = NPC(ID=ObjectId("668ca09b8b1a765fbab94e9c"))
 
-    while True:
-        prompt = input("Prompt: ")
-        ans = bot.qa(prompt, prompter, responder)
-        print(ans)
+    # while True:
+    #     prompt = input("Prompt: ")
+    #     ans = bot.generate_response(prompt, prompter, responder)
+    #     print(ans)
+
+    session_id = "66997d89afca9066c9e3180466997d86afca9066c9e31803"
+    collection_name = "Memories"
+    message_history = MongoDBChatMessageHistory(
+        session_id=session_id,
+        connection_string=URI,
+        database_name=DB_NAME,
+        collection_name=collection_name,
+    )
+    history = message_history.messages
+    print(history)
